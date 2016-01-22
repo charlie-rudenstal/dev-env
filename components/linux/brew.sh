bc18cd27b878a8ba7f5016decdc3812101cbd9f1
charud <charud@spotify.com>
2016-01-22 14:29:26 +0100

    Add brew to path

--- components/linux/brew.sh
+++ components/linux/brew.sh
@@ -1,2 +1,4 @@
 # homebrew linux
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
+
+echo "export PATH=\"/home/charud/.linuxbrew/bin:\$PATH\"" >> ~/.bashrc
--- components/osx/brew.sh
+++ components/osx/brew.sh
@@ -1,2 +1,4 @@
 # homebrew osx
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "export PATH=\"/home/charud/.linuxbrew/bin:\$PATH\"" >> ~/.bashrc
