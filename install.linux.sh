2d86a35b97efd2c49c7e12d7ddc7ab6c9c4f21ee
charud <charud@spotify.com>
2016-01-22 13:02:33 +0100

    Run vim PluginInstall

--- install.linux.sh
+++ install.linux.sh
@@ -22,6 +22,7 @@ brew install vim
 mkdir -p ~/.vim/bundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 wget -O ~/.vimrc https://gist.githubusercontent.com/charlie-rudenstal/09ea0a216e01ebc6440d/raw/.vimrc
+vim +PluginInstall +qall
 
 # autojump
 brew install autojump
